001eef9fbf73cb17d0bf9cbe9b18dade9c85df3d
BhadraMohit09 <mohitbhadra27@gmail.com>
2025-02-18 17:57:18 +0530

    Add project files.

---
+++ CustomTagHelper_MAB/TagHelpers/MyTagHelperA.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CustomTagHelper_MAB.TagHelpers
+{
+    [HtmlTargetElement("mytag")]
+    public class MyTagHelperA : TagHelper
+    {
+        public string path { get; set; }
+
+        public string altText { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("href", path);
+            output.Attributes.SetAttribute("alt", altText);
+            output.Content.SetContent("Click me");
+        }
+
+    }
+
+}
---
+++ CustomTagHelper_MAB/TagHelpers/MyTagHelperB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CustomTagHelper_MAB.TagHelpers
+{
+    [HtmlTargetElement("mybutton", Attributes = "bg-color")]
+    public class MyTagHelperB : TagHelper
+    {
+        public string BgColor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("style", $"background-color: black; color: white; padding: 10px 20px; border: none; color: white; cursor: pointer;");
+            output.Attributes.SetAttribute("onclick", $"document.body.style.backgroundColor = '{BgColor}'");
+
+            output.Content.SetContent("Click me");
+        }
+    }
+}
